f6fbb89a7a11853f253c9b3ad1307cc3732b04a9
Andreu Boada de Atela <andreuboada@me.com>
2018-01-15 00:54:18 -0600

    Update _deploy.sh

--- _deploy.sh
+++ _deploy.sh
@@ -1,19 +1,30 @@
 #!/bin/sh
 
+echo "P0"
 set -e
-
+echo "P1"
 [ -z "${GITHUB_PAT}" ] && exit 0
+echo "P2"
 [ "${TRAVIS_BRANCH}" != "master" ] && exit 0
 
+echo "P3"
 git config --global user.email "andreuboada@me.com"
 git config --global user.name "andreuboada"
 
+echo "P4"
 git clone -b gh-pages https://${GITHUB_PAT}@github.com/${TRAVIS_REPO_SLUG}.git book-output
 cd book-output
-git rm -rf *
+echo "P5"
+git rm -rf
+echo "P6"
+git status
+echo $(ls)*
+echo "P7"
 cp -r ../docs/* ./
+echo "P8"
 echo $(ls)
 git add --all *
+echo "P9"
 git commit -m "Actualizar notas" || true
 echo "Push to corresponding github branch"
 git push origin gh-pages
