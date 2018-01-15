e01f3af00cd3c083dabc4ddc3eaf2b5f2e3d42e0
Andreu Boada de Atela <andreuboada@me.com>
2018-01-15 00:31:03 -0600

    Update _deploy.sh

--- _deploy.sh
+++ _deploy.sh
@@ -10,7 +10,8 @@ git config --global user.name "andreuboada"
 
 git clone -b gh-pages https://${GITHUB_PAT}@github.com/${TRAVIS_REPO_SLUG}.git book-output
 cd book-output
+git rm -rf *
 cp -r ../est-aplicada-3-2018/docs/* ./
 git add --all *
 git commit -m "Actualizar notas" || true
-git push -q origin gh-pages
+git push origin gh-pages
